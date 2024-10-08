cee6e8f40711883bb0f44738e1b7c5023c91ea08
Ivan Arcuschin <iarcuschin@gmail.com>
2024-10-08 12:24:02 +0000

    Compute mean diff direction in probing notebook

--- notebooks/probing.py
+++ notebooks/probing.py
@@ -1,6 +1,7 @@
 # %%
 import os
 import pickle
+import torch
 
 os.environ["HF_HOME"] = "/workspace/hf_cache/"
 os.environ["HF_DATASETS_CACHE"] = "/workspace/hf_cache/"
@@ -17,4 +18,38 @@ with open(activations_results_path, "rb") as f:
     activations = pickle.load(f)
 
 activations
+# %%
+# Figure out how many positive and negatie examples
+
+positive_activations = []
+negative_activations = []
+for question_idx, question in enumerate(activations.eval_results.questions):
+    acts = activations.activations_by_question[
+        question_idx
+    ].activations  # Shape: [n_layers locs d_model]
+
+    # Take mean across locs
+    mean_acts = acts.mean(1)  # Shape: [n_layers d_model]
+
+    if question.is_correct:
+        positive_activations.append(mean_acts)
+    else:
+        negative_activations.append(mean_acts)
+
+# Stack them up
+positive_activations = torch.stack(
+    positive_activations
+)  # Shape: [n_positive n_layers d_model]
+negative_activations = torch.stack(
+    negative_activations
+)  # Shape: [n_negative n_layers d_model]
+
+# Mean across positive and negative examples
+positive_activations = positive_activations.mean(0)  # Shape: [n_layers d_model]
+negative_activations = negative_activations.mean(0)  # Shape: [n_layers d_model]
+
+# %%
+# Compute the difference
+
+diff = positive_activations - negative_activations  # Shape: [n_layers d_model]
 # %%
