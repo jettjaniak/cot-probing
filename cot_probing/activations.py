e3262ffa8713e93b4c794bf0cd982a2cfc86a2d7
Ivan Arcuschin <iarcuschin@gmail.com>
2024-10-11 18:53:06 +0000

    Working script to collect activations

--- cot_probing/activations.py
+++ cot_probing/activations.py
@@ -1,29 +1,16 @@
 from functools import partial
 
-from cot_probing.eval import EvalQuestion, EvalResults
 from cot_probing.typing import *
 from cot_probing.typing import Float, torch
 
 
 @dataclass
-class QuestionActivations:
-    activations: Float[torch.Tensor, "n_layers locs d_model"]
-    sorted_locs: list[int]
+class ActivationsInLayer:
+    activations: Float[torch.Tensor, "pos d_model"]
 
     def __repr__(self):
         activations_str = f"activations={list(self.activations.shape)}"
-        sl_first, sl_last = self.sorted_locs[0], self.sorted_locs[-1]
-        sorted_locs_str = (
-            f"sorted_locs=[{sl_first}, ..., {sl_last}] ({len(self.sorted_locs)})"
-        )
-        return f"QuestionActivations({activations_str}, {sorted_locs_str})"
-
-
-@dataclass
-class Activations:
-    eval_results: EvalResults
-    activations_by_question: list[QuestionActivations]
-    layers: list[int]
+        return f"QuestionActivations({activations_str})"
 
 
 def clean_run_with_cache(
@@ -70,40 +57,40 @@ def clean_run_with_cache(
     return resid_acts
 
 
-def get_mean_activations_by_common_token(
-    common_tokens: list[int], activations: Activations, q_idxs: list[int]
-) -> dict[int, Float[torch.Tensor, "layer model"]]:
-    q_act_by_q = activations.activations_by_question
-    eval_results = activations.eval_results
-    acts_list_by_tok = {t: [] for t in common_tokens}
-    for q_idx in q_idxs:
-        q_act: QuestionActivations = q_act_by_q[q_idx]
-        eval_q: EvalQuestion = eval_results.questions[q_idx]
-        # in general, the setup was that you can have many keys in
-        # eval_q.locs, and q_act.sorted_locs would contain unique
-        # sorted locations for all the keys in eval_q.locs
-        # but for now we only have "response"
-        assert eval_q.locs["response"] == q_act.sorted_locs
-        # shape: layers, locs, model
-        acts = q_act.activations
-        # all_locs and all_toks correspond to 2nd dim of acts
-        all_locs = q_act.sorted_locs
-        all_toks = torch.tensor([eval_q.tokens[loc] for loc in all_locs])
-        for t in common_tokens:
-            this_tok_mask = all_toks == t
-            if not this_tok_mask.any():
-                continue
-            # append activations from this question/response that
-            # correspond to this token to a list for this token
-            # acts shape: layers, locs, model
-            acts_this_tok = acts[:, this_tok_mask, :]
-            acts_list_by_tok[t].append(acts_this_tok)
-    # concatenate all the activations corresponding to each token
-    # dim 1 is the locs/tokens dimension
-    acts_by_tok = {
-        t: torch.cat(acts_list, dim=1) for t, acts_list in acts_list_by_tok.items()
-    }
-    # check if we get some acts for each token
-    assert all(acts_by_tok[t].numel() > 0 for t in common_tokens)
-    # take the mean over all questions for each token
-    return {t: acts.mean(1) for t, acts in acts_by_tok.items()}
+# def get_mean_activations_by_common_token(
+#     common_tokens: list[int], activations: Activations, q_idxs: list[int]
+# ) -> dict[int, Float[torch.Tensor, "layer model"]]:
+#     q_act_by_q = activations.activations_by_question
+#     eval_results = activations.eval_results
+#     acts_list_by_tok = {t: [] for t in common_tokens}
+#     for q_idx in q_idxs:
+#         q_act: QuestionActivations = q_act_by_q[q_idx]
+#         eval_q: EvalQuestion = eval_results.questions[q_idx]
+#         # in general, the setup was that you can have many keys in
+#         # eval_q.locs, and q_act.sorted_locs would contain unique
+#         # sorted locations for all the keys in eval_q.locs
+#         # but for now we only have "response"
+#         assert eval_q.locs["response"] == q_act.sorted_locs
+#         # shape: layers, locs, model
+#         acts = q_act.activations
+#         # all_locs and all_toks correspond to 2nd dim of acts
+#         all_locs = q_act.sorted_locs
+#         all_toks = torch.tensor([eval_q.tokens[loc] for loc in all_locs])
+#         for t in common_tokens:
+#             this_tok_mask = all_toks == t
+#             if not this_tok_mask.any():
+#                 continue
+#             # append activations from this question/response that
+#             # correspond to this token to a list for this token
+#             # acts shape: layers, locs, model
+#             acts_this_tok = acts[:, this_tok_mask, :]
+#             acts_list_by_tok[t].append(acts_this_tok)
+#     # concatenate all the activations corresponding to each token
+#     # dim 1 is the locs/tokens dimension
+#     acts_by_tok = {
+#         t: torch.cat(acts_list, dim=1) for t, acts_list in acts_list_by_tok.items()
+#     }
+#     # check if we get some acts for each token
+#     assert all(acts_by_tok[t].numel() > 0 for t in common_tokens)
+#     # take the mean over all questions for each token
+#     return {t: acts.mean(1) for t, acts in acts_by_tok.items()}
--- scripts/collect_activations.py
+++ scripts/collect_activations.py
@@ -1,77 +1,165 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import pickle
+from typing import Literal
 
 import torch
 from tqdm.auto import tqdm
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from cot_probing.activations import (
-    Activations,
-    QuestionActivations,
-    clean_run_with_cache,
-)
+from cot_probing.activations import ActivationsInLayer, clean_run_with_cache
+from cot_probing.eval import EvalQuestion
+from cot_probing.task import INSTRUCTION_STR
 from cot_probing.typing import *
+
+LocTypeToCache = Literal["resp", "instr", "quest", "fsp"]
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "eval_results_path",
+        "--eval-questions",
+        "-f",
         type=str,
-        help="Path to the evaluation results pickle file",
+        default=None,
+        help="Absolute path to eval_questions.pkl file",
     )
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def process_questions(
+    output_folder: str,
+    biased: bool,
+    model: AutoModelForCausalLM,
+    tokenizer: AutoTokenizer,
+    eval_questions: list[EvalQuestion],
+    loc_types_to_cache: set[LocTypeToCache] = {"resp"},
+):
+    if biased:
+        output_folder = os.path.join(output_folder, "biased_context")
+    else:
+        output_folder = os.path.join(output_folder, "unbiased_context")
 
-    args = parse_arguments()
-    eval_results_path = args.eval_results_path
+    # Load few-shot prompts
+    fsps_path = os.path.join(output_folder, "tokenized_fsps.pkl")
+    with open(fsps_path, "rb") as f:
+        fsps = pickle.load(f)
 
-    with open(eval_results_path, "rb") as f:
-        eval_results = pickle.load(f)
-
-    model_name = eval_results.model_name
-    task_name = eval_results.task_name
-    seed = eval_results.seed
-    num_samples = eval_results.num_samples
-    questions = eval_results.questions
-
-    print(
-        f"Processing {model_name} on {task_name} with seed {seed} and {num_samples} samples"
-    )
-
-    model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
+    # Load tokenized responses
+    tokenized_responses_path = os.path.join(output_folder, "tokenized_responses.pkl")
+    with open(tokenized_responses_path, "rb") as f:
+        tokenized_responses = pickle.load(f)
 
     layers_to_cache = list(range(model.config.num_hidden_layers))
-    activations_by_question: list[QuestionActivations] = []
-    for question in tqdm(questions):
-        locs_to_cache = set()
-        for key, locs in question.locs.items():
-            locs_to_cache.update(locs)
-        locs_to_cache = sorted(locs_to_cache)
+    activations_by_layer: dict[int, list[ActivationsInLayer]] = {
+        layer: [] for layer in layers_to_cache
+    }
 
-        # # hack to add "Let's think step by step"
-        # min_loc = locs_to_cache[0]
-        # # hack to not add anything else
-        # locs_to_cache = list(range(min_loc - 10, min_loc))  # + locs_to_cache
-        # # hack to have smaller prompt and faster inference
-        # input_ids = torch.tensor(question.tokens[:min_loc])
-        input_ids = torch.tensor(question.tokens)
+    tokenized_instruction = tokenizer.encode(
+        f"\n\n{INSTRUCTION_STR}\n", add_special_tokens=False, return_tensors="pt"
+    )[0]
+
+    for q_idx, question in tqdm(enumerate(eval_questions)):
+        tokenized_fsp = torch.tensor(fsps[q_idx])
+        tokenized_question = torch.tensor(question.tokenized_question)
+        tokenized_response = torch.tensor(tokenized_responses[q_idx])
+
+        # Build input tokens
+        input_ids = torch.cat(
+            (
+                tokenized_fsp,
+                tokenized_question,
+                tokenized_instruction,
+                tokenized_response,
+            ),
+            dim=0,
+        )
+
+        locs_to_cache = []
+        for loc_type in loc_types_to_cache:
+            if loc_type == "resp":
+                locs_to_cache.extend(
+                    range(
+                        len(tokenized_fsp)
+                        + len(tokenized_question)
+                        + len(tokenized_instruction),
+                        len(input_ids),
+                    )
+                )
+            elif loc_type == "instr":
+                locs_to_cache.extend(
+                    range(
+                        len(tokenized_fsp) + len(tokenized_question),
+                        len(tokenized_fsp)
+                        + len(tokenized_question)
+                        + len(tokenized_instruction),
+                    )
+                )
+            elif loc_type == "quest":
+                locs_to_cache.extend(
+                    range(
+                        len(tokenized_fsp), len(tokenized_fsp) + len(tokenized_question)
+                    )
+                )
+            elif loc_type == "fsp":
+                locs_to_cache.extend(range(len(tokenized_fsp)))
+
         resid_acts = clean_run_with_cache(
             model, input_ids, layers_to_cache, locs_to_cache
         )
 
-        activations_by_question.append(QuestionActivations(resid_acts, locs_to_cache))
+        for layer_idx, layer in enumerate(layers_to_cache):
+            activations_by_layer[layer].append(resid_acts[layer_idx].cpu())
 
-    model_name = model_name.replace("/", "--")
-    resid_acts_path = (
-        f"results/activations_{model_name}_{task_name}_S{seed}_N{num_samples}.pkl"
-    )
+    # Create dir for activations
+    acts_type = "+".join(list(loc_types_to_cache))
+    acts_name = f"acts_{acts_type}"
+    acts_folder_path = os.path.join(output_folder, acts_name)
+    os.makedirs(acts_folder_path, exist_ok=True)
 
-    with open(resid_acts_path, "wb") as f:
-        pickle.dump(
-            Activations(eval_results, activations_by_question, layers_to_cache), f
+    # Dump to disk
+    for layer, activations in activations_by_layer.items():
+        acts_path = os.path.join(acts_folder_path, f"L{layer}.pkl")
+        with open(acts_path, "wb") as f:
+            pickle.dump(activations, f)
+
+
+def main():
+    args = parse_arguments()
+
+    # Check that the eval_questions file was provided and exists
+    if args.eval_questions is None or not os.path.exists(args.eval_questions):
+        raise ValueError(
+            "Please provide a valid eval_questions file with the flag --eval-questions or -f"
         )
+
+    with open(args.eval_questions, "rb") as f:
+        eval_questions = pickle.load(f)
+
+    print(f"Processing {len(eval_questions)} questions from {args.eval_questions}...")
+
+    # Split the path to get details
+    parts = args.eval_questions.split("/")
+    misc_details = parts[-2]
+    print(f"Misc details: {misc_details}")
+    task = parts[-3]
+    print(f"Task: {task}")
+    model_name = parts[-4].replace("--", "/")
+    print(f"Model: {model_name}")
+
+    # Output folder is parent of eval_questions file
+    output_folder = "/".join(parts[:-1])
+    print(f"Output folder: {output_folder}")
+
+    model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
+    model.eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    for biased in [True, False]:
+        process_questions(output_folder, biased, model, tokenizer, eval_questions)
+
+
+if __name__ == "__main__":
+    main()
