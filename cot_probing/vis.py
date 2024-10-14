b0872192ce50c0b0d10f9cef4a1fea2b6ff34422
Jett Janiak <jettjaniak@gmail.com>
2024-10-14 17:46:33 +0000

    wip

--- cot_probing/vis.py
+++ cot_probing/vis.py
@@ -1,42 +1,33 @@
 import math
 
+from matplotlib import cm
+from matplotlib.colors import Normalize
 from transformers import PreTrainedTokenizerBase
-
-
-def single_probe_act_to_color(probe_act: float, scale) -> str:
-    def sigmoid(x: float) -> float:
-        return 1 / (1 + math.exp(-x / scale))
-
-    scaled_probe_act = sigmoid(probe_act)  # scale to 0-1
-
-    if scaled_probe_act < 0.5:  # red
-        red_val = 255
-        green_blue_val = min(int(255 * 2 * scaled_probe_act), 255)
-        return f"rgb({red_val}, {green_blue_val}, {green_blue_val})"
-    else:  # green
-        green_val = 255
-        red_blue_val = min(int(255 * 2 * (1 - scaled_probe_act)), 255)
-        return f"rgb({red_blue_val}, {green_val}, {red_blue_val})"
 
 
 def visualize_tokens_html(
     token_ids: list[int],
     tokenizer: PreTrainedTokenizerBase,
-    token_values: list[float | int],
-    values_scale: float = 300,
+    token_values: list[float] | None = None,
+    vmin: float | None = None,
+    vmax: float | None = None,
 ) -> str:
-    if len(token_ids) != len(token_values):
+    if token_values and len(token_ids) != len(token_values):
         raise ValueError(
             "The number of token IDs must match the number of token values."
         )
 
     token_htmls = []
-    for token_id, value in zip(token_ids, token_values):
-        str_token = tokenizer.decode(token_id).replace(" ", "&nbsp;")
-        str_token = (
-            str_token.replace("<", "&lt;").replace(">", "&gt;").replace("\n", r"\n")
-        )
-        bg_color = single_probe_act_to_color(float(value), values_scale)
+    for i in range(len(token_ids)):
+        token_id = token_ids[i]
+        if token_values is None:
+            bg_color = "rgb(255, 255, 255)"
+            title_str = ""
+        else:
+            value = token_values[i]
+            norm_value = (value - vmin) / (vmax - vmin)
+            bg_color = f"rgb(255, {int(255 * norm_value)}, {int(255 * norm_value)})"
+            title_str = f" title='{value:.2f}'"
         style = {
             "display": "inline-block",
             "border": "1px solid #888",
@@ -47,9 +38,13 @@ def visualize_tokens_html(
             "margin": "0 0 2px -1px",
             "padding": "0",
         }
+        str_token = tokenizer.decode(token_id).replace(" ", "&nbsp;")
+        str_token = (
+            str_token.replace("<", "&lt;").replace(">", "&gt;").replace("\n", r"\n")
+        )
 
         style_str = "; ".join(f"{k}: {v}" for k, v in style.items())
-        token_html = f"<div style='{style_str}' title='{value:.2f}'>{str_token}</div>"
+        token_html = f"<div style='{style_str}'{title_str}>{str_token}</div>"
         token_htmls.append(token_html)
         newlines = str_token.count(r"\n")
         token_htmls.extend(["<br>"] * newlines)
