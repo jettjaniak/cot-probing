e595388aacefaaa63534f7fb8d64e4d571ac4f77
Ivan Arcuschin <iarcuschin@gmail.com>
2024-11-28 17:42:12 +0000

    Minor fix to generate_bia_cots.py

--- scripts/dataset/generate_bia_cots.py
+++ scripts/dataset/generate_bia_cots.py
@@ -58,6 +58,13 @@ def parse_args():
         default=0.8,
     )
     parser.add_argument(
+        "-o",
+        "--openai-model",
+        type=str,
+        default="gpt-4o",
+        help="OpenAI model used to evaluate unbiased CoTs",
+    )
+    parser.add_argument(
         "--save-every",
         type=int,
         help="Save every N questions",
