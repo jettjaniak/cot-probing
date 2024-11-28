5a8e56e92d4e8ee63438a3891d64f8ac51b0209c
Ivan Arcuschin <iarcuschin@gmail.com>
2024-11-28 11:46:11 +0000

    Fixed script for evaluating no-cot accuracy

--- scripts/evaluate_no_cot_accuracy.py
+++ scripts/evaluate_no_cot_accuracy.py
@@ -13,7 +13,7 @@ from transformers import PreTrainedModel, PreTrainedTokenizerBase
 from cot_probing import DATA_DIR
 from cot_probing.activations import build_fsp_cache
 from cot_probing.diverse_combinations import load_and_process_file
-from cot_probing.questions_generation import generate_unbiased_few_shot_prompt
+from cot_probing.qs_generation import Question, generate_unbiased_few_shot_prompt
 from cot_probing.utils import load_model_and_tokenizer, setup_determinism
 
 
@@ -81,39 +81,37 @@ def get_no_cot_accuracy(
 def evaluate_no_cot_accuracy(
     model: PreTrainedModel,
     tokenizer: PreTrainedTokenizerBase,
-    question_dataset: list[dict],
+    question_dataset: dict[str, Question],
     unbiased_fsp_without_cot: str,
     fsp_size: int,
     seed: int,
 ):
     unbiased_no_cot_cache = build_fsp_cache(model, tokenizer, unbiased_fsp_without_cot)
 
-    results = []
-    for q in tqdm(question_dataset, desc="Evaluating no-CoT accuracy"):
-        question = q["question"]
-        expected_answer = q["expected_answer"]
+    results = {}
+    for q_id, q in tqdm(question_dataset.items(), desc="Evaluating no-CoT accuracy"):
+        question = q.question
+        expected_answer = q.expected_answer
         assert question.endswith("?")
+        assert "Question: " not in question
+        assert "Let's think step by step:" not in question
 
         no_cot_acc = get_no_cot_accuracy(
             model=model,
             tokenizer=tokenizer,
-            question_without_cot=f"{question}\nAnswer:",
+            question_without_cot=f"{q.with_question_prefix()}\nAnswer:",
             expected_answer=expected_answer,
             unbiased_no_cot_cache=unbiased_no_cot_cache,
         )
 
-        if "no_cot_accuracy" not in q:
-            q["no_cot_accuracy"] = []
-
-        q["no_cot_accuracy"].append(
+        results[q_id] = [
             {
-                "acc": no_cot_acc,
+                "no_cot_acc": no_cot_acc,
                 "model": model.config._name_or_path,
                 "fsp_size": fsp_size,
                 "seed": seed,
             }
-        )
-        results.append(q)
+        ]
 
     return results
 
